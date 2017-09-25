fbe613393962a162adfde9e9dea7a1e896612a23
takahisa <t_suzawa@opt.co.jp>
2017-09-26 00:04:25 +0900

    mailのタイトルに配信日をついか

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,7 +1,11 @@
+require 'date'
+
 class ReportMailer < ApplicationMailer
   default from: "from@example.com"
 
   def report_email
+    day = Date.today
+    @report = Report.find_by('deliver_date >= ?', day)
     mail to: ENV['ML_TO'], subject: '[週間セキュリ]'
   end
 end
